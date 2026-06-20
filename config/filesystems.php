655466d62386cf67f64149876f1f1f26363e31b0
Shuhrat Odilbekov <shuhratodilbekov3@gmail.com>
2026-06-20 16:01:38 +0500

    Fix: disable S3 ACL headers for GCS Uniform Bucket-Level Access compatibility

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,6 +58,10 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,
             'report' => false,
+            'visibility' => 'noPredefinedVisibility',
+            'options' => [
+                'ACL' => '',
+            ],
         ],
 
         'gcs' => [
