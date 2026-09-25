e75a6eb0724270b1f128555ff4127f20d3209b53
Shuhrat Odilbekov <shuhratodilbekov3@gmail.com>
2026-09-25 19:43:16 +0500

    build: track compiled assets in git

---
+++ public/build/assets/mail-Wr5_lQyG.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-Dvk3L-GP.js";var t=e(`ArrowLeft`,[[`path`,{d:`m12 19-7-7 7-7`,key:`1l729n`}],[`path`,{d:`M19 12H5`,key:`x3x0zl`}]]),n=e(`Mail`,[[`rect`,{width:`20`,height:`16`,x:`2`,y:`4`,rx:`2`,key:`18n3k1`}],[`path`,{d:`m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7`,key:`1ocrg3`}]]);export{t as n,n as t};
